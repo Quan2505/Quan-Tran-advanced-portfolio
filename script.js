d6a76c8e474f22577be3c6c5481ee5a22e0b1752
Quan2505 <qt148967@gmail.com>
2024-08-26 12:20:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var type = new Typed(".typing",{
+    strings:["Web Designer","Student at CU Boulder", "Mathematic major", "Former Vice President of Investment Club in high school"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
